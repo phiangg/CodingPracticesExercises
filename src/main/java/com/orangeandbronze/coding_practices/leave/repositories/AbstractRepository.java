4be47808ec6981b2b014fbdf37a1afc9df6d0862
Phiangg <sophiaquizon1102@gmail.com>
2023-05-04 14:41:05 +0800

    Added code for AbstractRepository.java

--- src/main/java/com/orangeandbronze/coding_practices/leave/repositories/AbstractRepository.java
+++ src/main/java/com/orangeandbronze/coding_practices/leave/repositories/AbstractRepository.java
@@ -1,4 +1,18 @@
 package com.orangeandbronze.coding_practices.leave.repositories;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
 public class AbstractRepository {
+
+    protected final Connection conn;
+
+    public AbstractRepository(){
+        try{
+            conn = DriverManager.getConnection("jdbc:hsqldb:leaves", "sa", "");
+        } catch (SQLException e){
+            throw new DataAccessException("Failed to connect to database", e);
+        }
+    }
 }
