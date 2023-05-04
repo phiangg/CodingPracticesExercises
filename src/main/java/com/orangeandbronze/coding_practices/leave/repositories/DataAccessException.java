609dfb0846928e39ea4fa0fe6d011962b8db0365
Phiangg <sophiaquizon1102@gmail.com>
2023-05-04 14:34:40 +0800

    Added code for DataAccessException.java

--- src/main/java/com/orangeandbronze/coding_practices/leave/repositories/DataAccessException.java
+++ src/main/java/com/orangeandbronze/coding_practices/leave/repositories/DataAccessException.java
@@ -1,4 +1,12 @@
 package com.orangeandbronze.coding_practices.leave.repositories;
 
-public class DataAccessException {
+public class DataAccessException extends RuntimeException {
+
+    public DataAccessException(String message){
+        super(message);
+    }
+
+    public DataAccessException(String message, Throwable cause){
+        super(message, cause);
+    }
 }
